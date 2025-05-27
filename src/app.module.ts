1725f7dc7fe73542ac55b6aea4b07b81338b4121
Sarnendu Mukherjee <sarnendu.hexa@gmail.com>
2025-05-27 16:16:51 +0200

    chore: remove boilerplate AppController and AppService along with their tests from the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SeasonsModule } from './seasons/seasons.module';
 import { RacesModule } from './races/races.module';
 import { DriversModule } from './drivers/drivers.module';
@@ -32,7 +30,7 @@ import configuration from './config/configuration';
     RacesModule,
     DriversModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
